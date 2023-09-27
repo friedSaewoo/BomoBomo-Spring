8d9afdc865e2260b099cd3b1f6cae0ada75e6a26
HyeongJunIm <chaesng6064@gmail.com>
2023-09-27 17:01:38 +0900

    버전 수정 v_1.5

--- src/main/java/com/example/bomobomo/mapper/MyPageMapper.java
+++ src/main/java/com/example/bomobomo/mapper/MyPageMapper.java
@@ -1,11 +1,15 @@
 package com.example.bomobomo.mapper;
 
+import com.example.bomobomo.domain.vo.MyPageSitterVo;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MyPageMapper {
 
     // 조회
+    public List<MyPageSitterVo> selectMyPageSitterList(MyPageSitterVo myPageSitterVo);
 
 
 
